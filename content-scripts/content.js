e4b06618ce895ced24ab542d1b7afdce018f0123
Robert Lin <rcl2106@gmail.com>
2021-07-11 23:36:02 -0400

    ✨ Add markdown formatting logic & robustify 'GitHub-Issue-page' detection

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,21 +1,75 @@
-console.log('>> GitHub Issues page detected! Run ext!', window.location.pathname)
+console.log('>> Initial load: GitHub Issues page detected! Run ext!', window.location.pathname)
+
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('js-repo-pjax-container')
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: false, childList: true, subtree: true }
+
+// Callback function to execute when mutations are observed
+let currentPath
+
+// If the targetNode is detected on this page, start observing it
+if (targetNode) {
+  const observer = new MutationObserver(
+    (mutationsList, observer) => {
+      if (currentPath !== window.location.pathname) {
+        currentPath = window.location.pathname
+        console.log('>> `location.pathname` has changed!', currentPath)
+    
+        // Check to see if this is a `GitHub issue`. If it is, trigger custom rendering logic
+        const m = window.location.pathname.match(/^\/(.*?)\/(.*?)\/issues\/(\d*)$/)
+        if (m?.[1]) {
+          console.log('\t>> This is a GitHub issue page:', m[1], m[2], m[3])
+          // console.log('>> discussions_bucket', document.getElementById('discussion_bucket'))
+          // console.log('>> mutationsList:', mutationsList)
+          // renderCustom(m[3])
+        }
+      }
+    })
+  observer.observe(targetNode, config)
+}
 
 // Write code that scans the DOM and replace all innerHTML of `<div class="edit-comment-hide"></div>` with
 // our own custom rendered content.
-if (document.getElementsByClassName('edit-comment-hide')[0]) {
-  const url = `https://api.github.com/repos${window.location.pathname}`
-  console.log('>> fetching:', url)
-  const req = fetch(url).then(rs => {
-    rs.json().then(dom =>{
-      console.log('>> issue body:', dom.body)
 
-      let newBody = dom.body.replace('\r\n', '<br /><br />')
-      newBody = newBody.replaceAll('- [ ]', '<input type="checkbox">&nbsp;')
-      newBody = newBody.replaceAll('[ ]', '<input type="checkbox">&nbsp;')
+const reLinks = /\[(.*?)\]\((.*?)\)/g
 
-      document.getElementsByClassName('edit-comment-hide')[0].innerHTML = `<div style="padding: 20px">${newBody}</div>`
+function renderCustom(cardNo) {
+  console.log('>> Calling renderCustom()', cardNo)
+  console.log("\t>> document.getElementsByClassName('edit-comment-hide')", document.getElementsByClassName('edit-comment-hide'))
+
+  if (document.getElementsByClassName('edit-comment-hide')[0]) {
+    const url = `https://api.github.com/repos${window.location.pathname}`
+    console.log('>> fetching:', url)
+    const req = fetch(url).then(rs => {
+      rs.json().then(dom =>{
+        console.log('>> issue body before:', JSON.stringify(dom.body))
+
+        let body = dom.body
+
+        body = body.replaceAll('- [ ]', '<input type="checkbox">&nbsp;')
+        body = body.replaceAll('[ ]', '<input type="checkbox">&nbsp;')
+
+        // Perform markdown formatting
+        // console.log('\t >>body:', body)
+        body = body.replaceAll(/### .*?\r\n/g, m => `<h3>${m.slice(4, -2)}</h3><br />`)
+        // body = body.replaceAll(/_.*_/g, '<em>$&</em>') // Almost works but doesn't eliminate the underscores
+        body = body.replaceAll(/_.*?_/g, m => `<i>${m.slice(1, -1)}</i>`)
+        body = body.replaceAll(/\*\*.*?\*\*/g, m => `<b>${m.slice(2, -2)}</b>`) // Use RegEx-- but need to escape!
+        body = body.replaceAll(/\n/g, '<br />')
+      
+        body = body.replace('\n', '<br />')
+
+        // Replace all links
+        body = body.replaceAll(reLinks, "<a target='_blank' href='$2'>$1</a>")
+        
+        console.log('>> issue body after:', JSON.stringify(body))
+
+        document.getElementsByClassName('edit-comment-hide')[0].innerHTML = `<div style="padding: 20px">${body}</div>`
+      })
     })
-  })
+  }
 }
 
 // This is code from the quickstart tutorial. Not part of my `GitHub Enhancer` extension
